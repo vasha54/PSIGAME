caf05e573ef8257b6b9cdfe4534279756072cb6f
vasha54 <luis.valido1989@gmail.com>
2023-10-01 08:21:07 -0400

    Commit inicial

---
+++ app/src/main/java/cu/innovat/psigameplus/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package cu.innovat.psigameplus.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import cu.innovat.psigameplus.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
