9381e603d072c226f03f20bdb51e1746103d48c0
vasha54 <luis.valido1989@gmail.com>
2023-12-30 17:43:54 -0500

    Subiendo primera version de la aplicación PSGAME++

--- app/src/main/java/cu/innovat/psigplus/util/Util.java
+++ app/src/main/java/cu/innovat/psigplus/util/Util.java
@@ -1,11 +1,15 @@
 package cu.innovat.psigplus.util;
 
+import android.graphics.Paint;
+import android.graphics.Rect;
+
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.UUID;
 import java.util.Locale;
 import java.util.regex.Pattern;
@@ -20,7 +24,8 @@ public class Util {
 
     private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
     private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
-    private static final Pattern CI = Pattern.compile("^\\d{11}$");
+    private static final Pattern CI = Pattern.compile("^\\d{11}");
+    public static final int WIDTH_TEMPLATE_CERTIFICATE = 546;
 
     public static String generateUUID(){
         UUID uuid = UUID.randomUUID();
@@ -42,7 +47,6 @@ public class Util {
 
     public static String convertToMMSS(int _second){
         return  String.format("%02d:%02d",0,_second);
-
     }
 
     public static String convertTo00(int _number){
@@ -57,7 +61,13 @@ public class Util {
 
     public static boolean isValidCI(String ci){
         boolean valid = true;
-        valid = CI.matcher(ci).matches();
+        if(ci.length()!=11) valid =false;
+
+        for(int i=0;i<ci.length() && valid ;i++){
+            if('0' <= ci.charAt(i) && ci.charAt(i)<='9') continue;
+            valid = false;
+        }
+       // valid = CI.matcher(ci).matches();
         if(valid){
             int year = Integer.parseInt(ci.substring(0,2));
             int month = Integer.parseInt(ci.substring(2,4));
@@ -79,9 +89,17 @@ public class Util {
         return valid;
     }
 
-    public static String convertToStringFillZeros(int number,int places){
+    public static String convertToStringFillSpace(int number,int places){
         String str = String.valueOf(number);
-        while(str.length()<places) str="0"+str;
+        while(str.length()<places) str=" "+str;
+        return str;
+    }
+
+    public static String convertToStringFillSpace(double number,int places,int decimals){
+        String format="%."+String.valueOf(decimals)+"f";
+        String str = String.format(format,number);
+        while(str.length()<places-1) str=" "+str;
+        str=str+"%";
         return str;
     }
 
@@ -90,5 +108,39 @@ public class Util {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String formatteDate = dateTime.format(formatter);
         return formatteDate;
+    }
+
+    public static int searchSizeOptimus(Paint paint, String text,int maxWidth , int maxHeigth){
+        int begin = 1;
+        int end =Integer.MAX_VALUE-2;
+        int pivot,best=begin;
+        while(begin<=end){
+            pivot = (begin+end)/2;
+            paint.setTextSize(pivot);
+            Rect bounds =new Rect();
+            int w = (int)paint.measureText(text);
+            paint.getTextBounds(text,0,text.length(),bounds);
+            int h = bounds.height();
+            if(w<=maxWidth && h<=maxHeigth){
+                best=Math.max(best,pivot);
+                begin=pivot+1;
+            }else{
+                end =pivot-1;
+            }
+        }
+        return best;
+    }
+
+    public static int [] generatePermutation(int N){
+        int [] permutations =new int[N+1];
+        for(int i=0;i<=N;i++) permutations[i]=i;
+        Random random = new Random();
+        for(int i=N;i>=0;i--){
+            int j = random.nextInt(i+1);
+            int tmp = permutations[i];
+            permutations[i] = permutations[j];
+            permutations[j] = tmp;
+        }
+        return permutations;
     }
 }
--- app/src/main/java/cu/innovat/psigplus/viewholder/ViewHolderStatisticsLevel.java
+++ app/src/main/java/cu/innovat/psigplus/viewholder/ViewHolderStatisticsLevel.java
@@ -18,6 +18,11 @@ public class ViewHolderStatisticsLevel extends RecyclerView.ViewHolder {
     TextView m_tvCountGame;
     TextView m_tvFirstGame;
     TextView m_tvLastGame;
+    TextView m_tvBestScore;
+    TextView m_tvWorstScore;
+    TextView m_tvMeanScore;
+    TextView m_tvGameBeforeWin;
+    TextView m_tvFirstWinGame;
 
     public ViewHolderStatisticsLevel(View itemView) {
         super(itemView);
@@ -25,6 +30,11 @@ public class ViewHolderStatisticsLevel extends RecyclerView.ViewHolder {
         m_tvCountGame = itemView.findViewById(R.id.v_count_games);
         m_tvFirstGame = itemView.findViewById(R.id.v_first_games);
         m_tvLastGame = itemView.findViewById(R.id.v_last_game);
+        m_tvBestScore = itemView.findViewById(R.id.v_best_game);
+        m_tvWorstScore = itemView.findViewById(R.id.v_worst_score);
+        m_tvMeanScore = itemView.findViewById(R.id.v_mean_score);
+        m_tvGameBeforeWin = itemView.findViewById(R.id.v_games_before_accept);
+        m_tvFirstWinGame = itemView.findViewById(R.id.v_date_first_accept);
 //        myTextView = itemView.findViewById(R.id.cmTextView);
 //        myImageView = itemView.findViewById(R.id.cmImageView);
     }
@@ -37,7 +47,7 @@ public class ViewHolderStatisticsLevel extends RecyclerView.ViewHolder {
 
     public void setCountGame(int countGame){
         if(m_tvCountGame != null){
-            m_tvCountGame.setText(Util.convertToStringFillZeros(countGame,10));
+            m_tvCountGame.setText(Util.convertToStringFillSpace(countGame,10));
         }
     }
 
@@ -50,6 +60,36 @@ public class ViewHolderStatisticsLevel extends RecyclerView.ViewHolder {
     public void setLastGame(long timestampLastGame){
         if(m_tvLastGame != null){
             m_tvLastGame.setText(Util.formatTimeStamp(timestampLastGame));
+        }
+    }
+
+    public void setBestScore(double bestScore){
+        if(m_tvBestScore != null){
+            m_tvBestScore.setText(Util.convertToStringFillSpace(bestScore,10,2));
+        }
+    }
+
+    public void setWorstScore(double worstScore){
+        if(m_tvWorstScore != null){
+            m_tvWorstScore.setText(Util.convertToStringFillSpace(worstScore,10,2));
+        }
+    }
+
+    public void setMeanScore(double meanScore){
+        if(m_tvMeanScore != null){
+            m_tvMeanScore.setText(Util.convertToStringFillSpace(meanScore,10,2));
+        }
+    }
+
+    public void setGameBeforeWin(int gameBeforeWin){
+        if(m_tvGameBeforeWin != null){
+            m_tvGameBeforeWin.setText(Util.convertToStringFillSpace(gameBeforeWin,10));
+        }
+    }
+
+    public void setFirstWinGame(long timestampFirstWinGame){
+        if(m_tvFirstWinGame != null){
+            m_tvFirstWinGame.setText(Util.formatTimeStamp(timestampFirstWinGame));
         }
     }
 }
